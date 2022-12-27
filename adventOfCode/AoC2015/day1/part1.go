9e9f759816a2028e22f5b97203eac5bc3c69cdd3
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-12-27 15:03:59 +0100

    lmao

--- adventOfCode/AoC2015/day1/part1.go
+++ adventOfCode/AoC2015/day1/part1.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// ( -> +1
+// ) -> -1
+
 func main() {
 
+	f, _ := os.Open("input.txt")
+	b := bufio.NewScanner(f)
+
+	var mappa = make(map[string]int)
+	for b.Scan() {
+
+		for i, k := range b.Text() {
+			mappa[string(k)]++
+			if (mappa["("]-mappa[")"]) == -1 {
+				fmt.Println(i + 1)
+				break
+			}
+
+		}
+
+	}
+	// fmt.Println(mappa["("] - mappa[")"])
 }
